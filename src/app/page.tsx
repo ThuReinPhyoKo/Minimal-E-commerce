90b5f53305e4c68d42a13dd4b385aa9c7df96930
ThuReinPhyoKo <118418687+ThuReinPhyoKo@users.noreply.github.com>
2026-02-22 16:53:24 +0630

    connection next/server

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "./components/hero/hero";
 import Grid from "./components/layout/grid";
 import ProductHeader from "./features/products/components/productHeader";
+import { connection } from "next/server";
 
-export default function Home() {
-  
+export default async function Home() {
+  await connection();
+
   return (
     <>
       <main className="bg-gray-50">
